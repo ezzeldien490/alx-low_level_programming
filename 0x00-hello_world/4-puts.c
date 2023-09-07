db392af6ef150dad923aa0e33cf2ffab09262040
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-07 16:59:22 -0700

    puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	const char *massage = "\"Programming is like building a multilingual puzzle\n";
+	const char *massage = "\"Programming is like building a multilingual puzzle";
+
 	puts(massage);
 
 	return (0);
-
 }

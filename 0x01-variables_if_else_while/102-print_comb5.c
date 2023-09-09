e75ae05896a186d9de1f2d6258f457071c9a45cf
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-09 05:47:22 -0700

    last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2, digit3, digit4;
+
+	while (digit1 <= 9)
+		digit2 = 0;
+	{
+		while (digit2 <= 9)
+			digit3 = digit1;
+		{
+			while (digit3 <= 9)
+				digit4 = 0;
+			{
+				while (digit4 <= 9)
+				{
+					if (digit4 > digit2)
+					{
+					putchar(digit1 + '0');
+					putchar(digit2 + '0');
+					putchar(' ');
+					putchar(digit3 + '0');
+					putchar(digit4 + '0');
+
+					if ((digit1 + digit2 + digit3 + digit4) != 35)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

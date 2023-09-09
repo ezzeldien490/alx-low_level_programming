57f20c2bdc399eaf9d603f72e3b881cc90872d45
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-08 17:52:22 -0700

    fifth task 2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,12 @@ int main(void)
 {
 	char letter = 'a';
 
-	while (letter <= 'z' && letter != ('q' && 'e'))
+	while (letter <= 'z')
 	{
+		if (letter != 'q' && letter != 'e')
+		{
 		putchar(letter);
+		}
 		letter++;
 	}
 	putchar('\n');

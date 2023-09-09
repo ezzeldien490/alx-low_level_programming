febe7b9d4a564afa3601f22930e7627ac9c811bc
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-08 17:59:55 -0700

    sixth tak

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 10)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}

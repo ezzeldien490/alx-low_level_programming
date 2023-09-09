ed5d9166ee06f2b4971a67f89af14cc8cd25c940
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-09 08:01:23 -0700

    last task 8

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			if (digit1 != digit2)
 			{
 				putchar((digit1 / 10) + '0');
-				putchar((digit1 & 10) + '0');
+				putchar((digit1 % 10) + '0');
 				putchar(' ');
 				putchar((digit2 / 10) + '0');
 				putchar((digit2 % 10) + '0');

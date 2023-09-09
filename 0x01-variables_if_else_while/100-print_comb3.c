bf55769927b90d8a672e391a9fe4027739a20dc2
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-09 02:59:52 -0700

    11th task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+
+		while (digit2 <= 9)
+		{
+			if (digit1 < digit2)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+			}
+
+			if ((digit1 + digit2) != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+}

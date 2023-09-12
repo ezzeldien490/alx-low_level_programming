c39de8cfbcf1b5b1fee463363707d00dcda2b8d4
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-12 09:12:04 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - print alphabet a-z
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int _putchar(char);
+void print_alphabet(void);
+
 
 #endif

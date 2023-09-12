470142d27dbc05c9a650961c8522147a6af6cff4
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-12 09:41:37 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}

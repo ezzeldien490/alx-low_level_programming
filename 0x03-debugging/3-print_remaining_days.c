11c1921e83affcdf2b4f01fa8e595e0f1e5cc2eb
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-13 12:34:44 -0700

    task3 2

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,16 +24,11 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
-		else
-		{
+		
+		
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
+		
 	}
 }
 

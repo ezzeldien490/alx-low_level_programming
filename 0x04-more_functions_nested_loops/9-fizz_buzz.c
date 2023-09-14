bbdca402a4a80b22002319b583b36d14b57dc7da
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-14 08:49:11 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n, d, c, a;
+
+	for (n = 1; n <=100; n++)
+	{
+		if ((n % 5) == 0 && (n % 3) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+		printf("%d ", n);
+		}
+	}
+	printf("\n");
+}

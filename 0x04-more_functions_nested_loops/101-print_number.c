87ecf33b518e9f23cb70dee55170b8a5964d06db
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-14 17:52:43 -0700

    task12 7

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,18 @@
 
 void print_number(int n)
 {
+	unsigned int d = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		d = -d;
 	}
 
-	if (n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else
-	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
-	}
+	if ((d / 10) > 0)
+		print_number(d / 10);
+
+
+	_putchar((d % 10) + '0');
+
 }

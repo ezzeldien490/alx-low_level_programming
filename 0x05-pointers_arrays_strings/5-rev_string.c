d61931d42a2fe01fc8f508cd9658b6955a28c1a5
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-18 15:02:54 -0700

    task5 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,13 @@
 void rev_string(char *s)
 {
 	int a;
+	int i = 0, r;
 	char b[1000];
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		b[a] = s[a];
 	}
-	int r;
-	int i = 0;
 	for (r = a - 1; r >= 0; r--)
 	{
 		s[r] = b[i];

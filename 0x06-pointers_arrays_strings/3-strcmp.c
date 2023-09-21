0d8ac082ed526a6f9c3c5e59b46ee5177e55f38a
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-21 05:50:27 -0700

    udpate3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -25,6 +25,14 @@ int _strcmp(char *s1, char *s2)
 			return (s1[i] - s2[i]);
 		}
 	}
+	if (a > b)
+        {
+                return (s1[b]);
+        }
+        else if (b < a)
+        {
+                return (s2[a] * -1);
+        }
 
 	return (0);
 }

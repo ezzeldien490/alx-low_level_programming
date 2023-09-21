7f0649c154da8659ab47a8ff5ae69bf32b1b8c61
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-21 06:37:06 -0700

    task5 3

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,13 +3,14 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
  *
- * @s: array.
+ * @b: array.
  *
  * Return: *s.
 */
 char *string_toupper(char *b)
 {
 	int i;
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] >= 'a' && b[i] <= 'z')

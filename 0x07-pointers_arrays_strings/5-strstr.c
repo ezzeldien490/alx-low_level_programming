35f9404949a7ef7cedb98c032284c4fdcb4805e9
ezzeldien490 <ezzeldien490@gmail.com>
2023-09-25 14:20:43 -0700

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Find first occurence of given string in another string
+ * @haystack: String to search through
+ * @needle: String to search for
+ *
+ * Return: Pointer to beginning of located string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *m;
+	for (; *haystack != 0; haystack++)
+	{
+		h = haystack;
+		m = needle;
+		for (; *h == *m && *h != '\0' &&
+				*m != '\0'; m++, h++)
+		{}
+		if (*m == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return ('\0');
+}

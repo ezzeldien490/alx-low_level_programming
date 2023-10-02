652dd009a840fba57b862b8a7b92249b5a833a6c
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-02 12:24:12 -0700

    task0 3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,8 @@
  *
  * Return: 0 for succesful exit
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }

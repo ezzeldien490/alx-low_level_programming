5a83ab1aa91e87fe0517b4a2c86991e2342f35e5
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-02 13:36:40 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	    if (argc > 1)
-	    {
-		    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	    }
-	    else
-	    {
-		    printf("Error\n");
-		    return (1);
-	    }
-
-	return (0);
+	if (argc > 2)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }

24ca4e8db83da744a705db31a44a82138396b8eb
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-02 12:16:42 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the executable file
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: 0 for succesful exit
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

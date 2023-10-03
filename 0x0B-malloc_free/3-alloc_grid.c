1d8dd0a4aba883e25862d22eaa3fd0a49fb111e1
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-03 14:09:25 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,4 +10,39 @@
  * Return: d.
 */
 
+int **alloc_grid(int width, int height)
+{
+	int **d;
+	int i, j;
 
+	if (width == '0' || height == '0')
+		return (NULL);
+
+	d = malloc(height * sizeof(int));
+
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+
+	for (i = 0; i < height; i++)
+	{
+		d[i] = malloc(width * sizeof(int));
+
+		if (d[i] == NULL)
+		{
+			i--;
+			for (; i >= 0;i--)
+			{
+				free(d[i]);
+			}
+			free(d);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			d[i][j] = 0; 
+	}
+	return (d);
+}

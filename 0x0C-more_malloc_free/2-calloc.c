eea2e2f58869a08b33125c81d7c7cd9dd92e2d52
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-07 14:34:41 -0700

    tsak2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc -  allocates memory for an array.
+ *
+ * @nmemb: array.
+ * @size: number of bytes.
+ *
+ * Return: nmemb.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, *d;
+
+	if (nmemb == '0' || size == '0')
+	{
+		return (NULL);
+	}
+
+	d = malloc(nmemb * size);
+
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		d[i] = '0';
+	}
+	return (d);
+}

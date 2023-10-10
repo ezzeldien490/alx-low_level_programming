cc41e762f0c4830f879f0779830b265ddcf3242b
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-10 13:33:43 -0700

    task4 2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,21 +7,24 @@
  * @name: name of new dog.
  * @age: age of new dog.
  * @owner: name of owner.
+ *
+ * Return: d.
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	
+
 	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
-		return (NULL);
-	else
 	{
+		return (NULL);
+	}
+
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
+
 	return (d);
 }

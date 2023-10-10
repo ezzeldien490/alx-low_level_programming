c60a96d283b29a2523968477b2bc7612210190b6
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-10 12:43:55 -0700

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog.
+ *
+ * #d: pointer.
+*/
+
+void print_dog(struct dog *d)
+{
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}

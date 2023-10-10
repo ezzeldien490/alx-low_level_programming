c23a7dfd2fa2c2f017eb6df3327182eec11edfc5
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-10 11:50:10 -0700

    tasak0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - print name, age , owner.
+ *
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner
+ *
+ * Description: anythine.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

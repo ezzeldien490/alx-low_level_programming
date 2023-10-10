f51d17aa4c0c7ee5f24a72d6b8364e573b79782a
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-10 13:13:49 -0700

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,12 @@
  * Description: anythine.
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);

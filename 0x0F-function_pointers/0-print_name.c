f90717c847d5cdd909fe6c6c7687e419f3e412d7
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-11 15:44:54 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - pritns a name.
+ *
+ * @name: name of  the person.
+ * @f: pointer to function.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

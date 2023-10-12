4161ba00ef3ee7bb733085f3bad90e85f5269495
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-12 13:20:36 -0700

    task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,19 @@
 
 #include <stdarg.h>
 
+typedef struct print_form
+{
+	char *c;
+	void (*f)();
+} print_form_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
+
 #endif

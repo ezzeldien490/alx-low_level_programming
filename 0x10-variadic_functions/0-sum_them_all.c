80cc8afa2b2cac6a1a981a5c88e70a8ea2c2b0c7
ezzeldien490 <ezzeldien490@gmail.com>
2023-10-12 13:34:01 -0700

    test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,9 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 		for (i = 0; i < n; i++)
-		{
 			total += va_arg(ap, int);
-		}
-	va_end(ap);
+			va_end(ap);
 	return (total);
 }

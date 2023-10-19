aab9b0d691757a88ba036ba8a86a7eb886a0c57e
Ezz Eldien Ayman <141520261+ezzeldien490@users.noreply.github.com>
2023-10-19 14:58:30 -0700

    task2 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
  * @str: date in new node.
  *
  * Return: The address of the new element, or null of it failed.
-*/
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
@@ -17,16 +17,19 @@ list_t *add_node(list_t **head, const char *str)
 	int size = strlen(str2);
 
 	list_t *temp = malloc(sizeof(list_t));
+
 	if (temp == NULL)
 	{
 		return (NULL);
 	}
+	else
+	{
+		temp->str = str2;
+		temp->len = size;
+		temp->next = *head;
 
-	temp->str = str2;
-	temp->len = size;
-	temp->next = *head;
+		*head = temp;
 
-	*head = temp;
-
-	return (temp);
+		return (temp);
+	}
 }

c541e3b5e31a7c13ba6b52519711cf026b740de8
Ezz Eldien Ayman <141520261+ezzeldien490@users.noreply.github.com>
2023-10-19 14:07:16 -0700

    task5

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,17 +14,14 @@ size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	list_t *ptr = malloc(sizeof(list_t));
-	*ptr = *h;
-
-	for (; ptr != NULL; count++)
+	for (; h != NULL; count++)
 	{
-		if (ptr->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", ptr->len, ptr->str);
+			printf("[%d] %s\n", h->len, h->str);
 
-		ptr = ptr->next;
+		h = h->next;
 	}
 	return (count);
 }

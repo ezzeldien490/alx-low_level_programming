d85f8f8a7277faae7f5fc9153c7ee86681a072f9
Ezz Eldien Ayman <141520261+ezzeldien490@users.noreply.github.com>
2023-10-19 16:19:33 -0700

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list.
+ *
+ * @head: linked list.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

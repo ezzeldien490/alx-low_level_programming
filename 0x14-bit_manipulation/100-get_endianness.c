245b00bb094b8d13c8bfe861d77e96bb8342aaea
ezzeldien490 <ezzeldien490@gmail.com>
2023-11-24 16:38:40 -0800

    tasks

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+/**
+ * get_endianness - Gets the endianness of a machine
+ *
+ * Return: 1 if little endian, 0 if big endian
+ */
+int get_endianness(void)
+{
+	int x;
+	char *y;
+
+	y = (char *)&x;
+	return (*y + 48);
+}

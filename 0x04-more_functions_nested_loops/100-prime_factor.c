2afe80738586620791a9537ddf06ad81450e0c7d
Ezz Eldien Ayman <ezzeldien490@gmail.com>
2024-02-21 12:41:09 -0800

    updated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,24 +8,19 @@
 int main(void)
 {
 	long int n = 612852475143, d, f;
-	 d = 3;
 	 f = 0;
 
-	while (n != 1)
+	for (d = 3; n != 1; )
 	{
 		if (n % d == 0)
 		{
 			n = n / d;
 
 			if (d > f)
-			{
 				f = d;
-			}
 		}
 		else
-		{
 			d++;
-		}
 	}
 	printf("%ld\n", f);
 	return (0);
